52e92a0d00cf4bdee23ea8d254d60c1b5dc38072
vinos ronra Shimray <ronravincent@gmail.com>
2018-02-19 01:26:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/ui/page1.html', function (req, res) {
+app.get('/page1', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'page1.html'));
 });
 
